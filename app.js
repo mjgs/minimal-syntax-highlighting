c1bfaab1b12091aaeca14c73903b1753b77f1907
Mark Smith <markjgsmith@gmail.com>
2022-12-14 12:19:34 +0700

    Initial files

---
+++ app.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+const debug = require('debug')('minimal-syntax-highlighting:app');
+
+const fse = require('fs-extra');
+const { readFile } = require('node:fs/promises');
+const path = require('path');
+const marked = require('marked');
+const hljs = require('highlight.js');
+
+async function load(filePath) {
+  return readFile(filePath, {
+    encoding: 'utf8'
+  });
+}
+
+async function write(filePath, data) {
+  return fse.outputFile(filePath, data);
+}
+
+console.log('starting...');
+
+// Read template
+const templatePath = path.join('index.md');
+const contents = await load(templatePath);
+
+// Render template
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  highlight: function(code, lang) {
+    const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+    return hljs.highlight(code, { language }).value;
+  },
+  langPrefix: 'hljs language-', // highlight.js css expects a top-level 'hljs' class.
+  pedantic: false,
+  gfm: true,
+  breaks: false,
+  sanitize: false,
+  smartypants: false,
+  xhtml: false
+});
+const html = marked.parse(contents);
+
+// Output template
+const outputPath = path.join('site', 'index.html');
+await write(outputPath, html);
+
+console.log('done');
---
+++ index.md
@@ -0,0 +1,10 @@
+# minimal-syntax-highlighting
+
+A code block:
+
+    function hw() {
+      console.log('Hello World'); 
+    }
+    hw();
+    
+That's all folks! 
